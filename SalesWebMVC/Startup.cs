b975d5ea20d076de706fa4eb4ea0f274bb22d604
rafaelainocencio <rafaelainocencioo@gmail.com>
2022-05-24 16:25:38 -0300

    Adaptando MySQL e Initial Migration

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -28,7 +28,9 @@ namespace SalesWebMVC
             services.AddControllersWithViews();
 
             services.AddDbContext<SalesWebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext")));
+                   options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMVC")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
